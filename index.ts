a5b085914829d57a009ac6c819e37ef7aa61b7d2
Alexey Chvarkov <chvarkov.alexey@gmail.com>
2020-03-12 21:12:25 +0000

    Added reexport file. Update version.

--- index.ts
+++
@@ -1,4 +0,0 @@
-export * from './src/google-recaptcha.module';
-export * from './src/decorators/recaptcha';
-export * from './src/guards/google-recaptcha.guard';
-export * from './src/interfaces/google-recaptcha-module-options';
