42cab357c379e4c63dce3ac18cd3bfefb311fc16
chvarkov <chvarkov.alexey@gmail.com>
2020-10-11 18:54:46 +0300

    Added reexport types.

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { Recaptcha } from './decorators/recaptcha';
 export { GoogleRecaptchaGuard } from './guards/google-recaptcha.guard';
 export { GoogleRecaptchaModule } from './google-recaptcha.module';
 export { GoogleRecaptchaModuleOptions } from './interfaces/google-recaptcha-module-options';
+export { ErrorCode } from './enums/error-code';
+export { ErrorHandler } from './types';
