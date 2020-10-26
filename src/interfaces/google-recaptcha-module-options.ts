945fbe0bb795de3a1e9f6bd9a1a4469153fa03d0
Nebojsa Cvetkovic <nebkat@gmail.com>
2020-10-26 23:48:53 +0000

    Async module initialization

--- src/interfaces/google-recaptcha-module-options.ts
+++ src/interfaces/google-recaptcha-module-options.ts
@@ -1,5 +1,16 @@
 import { GoogleRecaptchaGuardOptions } from './google-recaptcha-guard-options';
 import { GoogleRecaptchaValidatorOptions } from './google-recaptcha-validator-options';
+import { ModuleMetadata, Type } from '@nestjs/common/interfaces';
 
-export interface GoogleRecaptchaModuleOptions extends GoogleRecaptchaValidatorOptions, GoogleRecaptchaGuardOptions {
+export interface GoogleRecaptchaModuleOptions extends GoogleRecaptchaValidatorOptions, GoogleRecaptchaGuardOptions {}
+
+export interface GoogleRecaptchaOptionsFactory {
+    createGoogleRecaptchaOptions(): Promise<GoogleRecaptchaModuleOptions> | GoogleRecaptchaModuleOptions;
+}
+
+export interface GoogleRecaptchaModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+    inject?: any[];
+    useClass?: Type<GoogleRecaptchaOptionsFactory>;
+    useExisting?: Type<GoogleRecaptchaOptionsFactory>;
+    useFactory?: (...args: any[]) => Promise<GoogleRecaptchaModuleOptions> | GoogleRecaptchaModuleOptions;
 }
