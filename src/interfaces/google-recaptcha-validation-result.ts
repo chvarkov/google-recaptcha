a5ed65f155abff77d8a625941842647778bf8d85
chvarkov <chvarkov.alexey@gmail.com>
2021-06-24 00:25:38 +0300

    WIP: Recaptcha verification result decorator.

--- src/interfaces/google-recaptcha-validation-result.ts
+++ src/interfaces/google-recaptcha-validation-result.ts
@@ -1,7 +1,10 @@
 import { ErrorCode } from '../enums/error-code';
+import { VerifyResponseV2 } from './verify-response';
 
-export interface GoogleRecaptchaValidationResult {
+export interface GoogleRecaptchaValidationResult extends VerifyResponseV2 {
 	success: boolean;
+	hostname: string;
+	action?: string;
 	score?: number;
 	errors: ErrorCode[];
 }
