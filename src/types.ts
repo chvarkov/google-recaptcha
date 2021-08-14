2f4941f9c664303ddd29a2c7527a902f2bcbf2d5
chvarkov <chvarkov.alexey@gmail.com>
2021-08-14 10:51:12 +0000

    Added auto detection request type.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
+import { ContextType } from '@nestjs/common';
+
 export type RecaptchaResponseProvider = (req) => string | Promise<string>;
 
 export type ScoreValidator = number | ((score: number) => boolean);
+
+export type RecaptchaContextType = ContextType | 'graphql';
